14d07f8d2b4353693ba6889a9f811c4d26d29ae8
academyitwill <academyitwill@gmail.com>
2022-07-04 12:53:15 +0900

    ..

--- src/com/team2/sbucks/유지훈/test/OrderDetailDaoTest.java
+++ src/com/team2/sbucks/유지훈/test/OrderDetailDaoTest.java
@@ -9,7 +9,7 @@ public class OrderDetailDaoTest {
 	public static void main(String[] args) throws Exception{
 		OrderListDao orderListDao = new OrderListDao();
 		ProductDao productDao = new ProductDao();
-		OrderDetail orderDetail = new OrderDetail(1, orderListDao.selectByNo(1), 1, 1, productDao.selectByNo(1));
+		//OrderDetail orderDetail = new OrderDetail(1, orderListDao.selectByNo(1), 1, 1, productDao.selectByNo(1));
 		
 		
 	
