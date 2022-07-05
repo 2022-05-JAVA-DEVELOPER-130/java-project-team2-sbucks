ca9dbee2039d7d54ab1266fb2cc944bcb20ef22b
ddoyoon <1026jsy@gmail.com>
2022-07-05 14:44:05 +0900

    ..

--- src/com/team2/sbucks/service/LoginService.java
+++ src/com/team2/sbucks/service/LoginService.java
@@ -1,5 +1,37 @@
 package com.team2.sbucks.service;
 
-public class LoginService {
+import com.team2.sbucks.dao.member.LoginDao;
+import com.team2.sbucks.dao.member.MemberDao;
 
+public class LoginService {
+	private LoginDao loginDao;
+	private MemberDao memberDao;
+	
+	public LoginService() {
+		loginDao = new LoginDao();
+		memberDao = new MemberDao();
+	}
+	
+
+	
+	/*
+	 * 비밀번호 확인 후 맞으면 비밀번호 변경
+	 */
+	public boolean CheckUpdatePassword (String newPassword, String oldPassword) throws Exception{
+		boolean isSuccess = false;
+		
+		if(loginDao.checkPassword(oldPassword)==true) {
+			
+			loginDao.updatePassword(newPassword, oldPassword);
+			isSuccess = true;
+			
+		}else {
+			isSuccess = false;
+			System.out.println("비밀번호가 틀렸습니다. 다시 입력해주세요");
+		}
+		
+		return isSuccess;
+	}
+	
+	
 }
