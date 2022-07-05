a0bd52d66a6b9fd8ae9b10268dc232576660c134
journeyjw <kjw0258@naver.com>
2022-07-05 16:15:25 +0900

    ..

---
+++ src/com/team2/sbucks/ui/orderPanel/cartPanel.java
@@ -0,0 +1,14 @@
+package com.team2.sbucks.ui.orderPanel;
+
+import javax.swing.JPanel;
+
+public class cartPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public cartPanel() {
+
+	}
+
+}
---
+++ src/com/team2/sbucks/ui/orderPanel/orderPanel.java
@@ -0,0 +1,14 @@
+package com.team2.sbucks.ui.orderPanel;
+
+import javax.swing.JPanel;
+
+public class orderPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public orderPanel() {
+
+	}
+
+}
