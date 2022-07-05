dd830d9c3dcbb164bf7e62be1bfdea134d8b5bfe
journeyjw <kjw0258@naver.com>
2022-07-06 00:31:24 +0900

    ..

--- src/com/team2/sbucks/ui/productPanel/ProductAllPanel.java
+++ src/com/team2/sbucks/ui/productPanel/ProductAllPanel.java
@@ -39,14 +39,14 @@ public class ProductAllPanel extends JPanel {
 	private JTextField productKalTF;
 	private JTextField productCaffineTF;
 	private JTextField productNaTF;
-<<<<<<< HEAD
 	private ProductDetail productDetail;
 	private ProductDetailPanel productDetailPanel;
+	private JLabel lblNewLabel_4;
+	private JLabel lblNewLabel_5;
+	private JLabel lblNewLabel_6;
 	
 
-=======
-	
->>>>>>> branch 'master' of https://github.com/2022-05-JAVA-DEVELOPER/java-project-team2-sbucks.git
+
 	/**
 	 * Create the panel.
 	 */
@@ -60,49 +60,44 @@ public class ProductAllPanel extends JPanel {
 		productListBasicpanel = new JPanel();
 		
 		productListBasicpanel.setForeground(new Color(46, 139, 87));
-		productListBasicpanel.setPreferredSize(new Dimension(320, 1000));
+		productListBasicpanel.setPreferredSize(new Dimension(320, 1500));
 		scrollPane.setViewportView(productListBasicpanel);
 		productListBasicpanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		/**********************************************************/
 		productitemPanel_1 = new JPanel();
-<<<<<<< HEAD
-		productitemPanel_1.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				
-				
-			}
-		});
-		productitemPanel_1.setPreferredSize(new Dimension(350, 130));
-=======
-		
-		productitemPanel_1.setPreferredSize(new Dimension(400, 130));
->>>>>>> branch 'master' of https://github.com/2022-05-JAVA-DEVELOPER/java-project-team2-sbucks.git
+
 		productListBasicpanel.add(productitemPanel_1);
 		productitemPanel_1.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel_6 = new JLabel("");
+		lblNewLabel_6.addMouseListener(new MouseAdapter() {
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		
+		lblNewLabel_6.setBounds(5, 5, 100, 100);
 		
 		
-		lblNewLabel.setIcon(new ImageIcon(ProductAllPanel.class.getResource("/images/(HOT)아메리카노.jpg")));
-
-		lblNewLabel.setBounds(12, 10, 107, 98);
-		productitemPanel_1.add(lblNewLabel);
+		lblNewLabel_6.setIcon(new ImageIcon(ProductAllPanel.class.getResource("/images/(HOT)아메리카노.jpg")));
+		productitemPanel_1.add(lblNewLabel_6);
 		
-		JLabel lblNewLabel_1 = new JLabel("(HOT)아메리카노");
-		lblNewLabel_1.setBounds(131, 10, 113, 15);
-		productitemPanel_1.add(lblNewLabel_1);
+		lblNewLabel_5 = new JLabel("(HOT)아메리카노");
+		lblNewLabel_5.setBounds(110, 46, 93, 17);
+		lblNewLabel_5.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+		productitemPanel_1.add(lblNewLabel_5);
 		
-		JLabel lblNewLabel_2 = new JLabel("<html>안녕<br>안녕</html>");
-		lblNewLabel_2.addMouseListener(new MouseAdapter() {
+		lblNewLabel_4 = new JLabel("<html>안녕<br>안녕</html>");
+		lblNewLabel_4.setBounds(208, 40, 22, 30);
+		lblNewLabel_4.setFont(new Font("맑은 고딕", Font.PLAIN, 11));
+		lblNewLabel_4.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				
 			
 			}
 		});
-		lblNewLabel_2.setBounds(120, 35, 218, 85);
-		productitemPanel_1.add(lblNewLabel_2);
+		productitemPanel_1.add(lblNewLabel_4);
 		
 		JLabel lblNewLabel_3 = new JLabel("전체상품");
 		lblNewLabel_3.setFont(new Font("맑은 고딕", Font.BOLD, 16));
@@ -123,6 +118,7 @@ public class ProductAllPanel extends JPanel {
 		
 		productListBasicpanel.removeAll();
 		for(int i=0;i<productList.size();i++) {
+			
 			Product product = productList.get(i);
 			productitemPanel_1 = new JPanel();
 			
@@ -131,23 +127,37 @@ public class ProductAllPanel extends JPanel {
 			productitemPanel_1.setLayout(null);
 			
 			JLabel lblNewLabel = new JLabel("");
-
-			lblNewLabel.setIcon(new ImageIcon(ProductAllPanel.class.getResource("/images/(HOT)아메리카노.jpg")));
-
+			lblNewLabel.addMouseListener(new MouseAdapter() {
+				
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					
+					ProductService productService= new ProductService();
+					try {
+						System.out.println(productService.selectByNo(product.getProduct_no()));
+						
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			});
 			
-			System.out.println(product.getProduct_name());
 			lblNewLabel.setIcon(new ImageIcon(ProductAllPanel.class.getResource("/images/"+product.getProduct_name()+".jpg")));
 
 
 			lblNewLabel.setBounds(12, 10, 107, 98);
+			
 			productitemPanel_1.add(lblNewLabel);
 			
 			JLabel lblNewLabel_1 = new JLabel(product.getProduct_name());
-			lblNewLabel_1.setBounds(131, 28, 113, 15);
+			lblNewLabel_1.setBounds(120, 20, 113, 15);
+			lblNewLabel_1.setFont(new Font("맑은 고딕", Font.BOLD, 12));
 			productitemPanel_1.add(lblNewLabel_1);
 			
 			JLabel lblNewLabel_2 = new JLabel("<html>"+product.getProduct_content()+"</html>");
-			lblNewLabel_2.setBounds(131, 35, 126, 73);
+			lblNewLabel_2.setBounds(120, 35, 231, 73);
+			lblNewLabel_2.setFont(new Font("맑은 고딕", Font.PLAIN, 11));
 			productitemPanel_1.add(lblNewLabel_2);
 			
 			JLabel lblNewLabel_3 = new JLabel("전체상품");
