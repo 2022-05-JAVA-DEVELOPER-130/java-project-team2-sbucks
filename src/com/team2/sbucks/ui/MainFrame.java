0bfc93b0eaf660e538cb225328540026478f3d64
journeyjw <kjw0258@naver.com>
2022-07-06 15:29:17 +0900

    ..

--- src/com/team2/sbucks/ui/MainFrame.java
+++ src/com/team2/sbucks/ui/MainFrame.java
@@ -132,9 +132,8 @@ public class MainFrame extends JFrame {
 		orderListPanel.setFrame(this);
 		orderPanel.setFrame(this);
 		cartPanel.setFrame(this);
-		loginPanel.setFrame(this);
 		joinPanel.setFrame(this);
-		updatePanel.setFrame(this);
+		
 		
 		
 		/****************************************************/
