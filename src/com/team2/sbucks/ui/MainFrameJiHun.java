2860dd1f11faacaf8fa23c89c2ba85101e0a18c0
예쁘고 당당한 지훈공주 <  @DESKTOP-FI0V939>
2022-07-06 12:29:08 +0900

    ..

---
+++ src/com/team2/sbucks/ui/MainFrameJiHun.java
@@ -0,0 +1,46 @@
+package com.team2.sbucks.ui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MainFrameJiHun extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrameJiHun frame = new MainFrameJiHun();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainFrameJiHun() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 400, 600);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		OrderPaneljiHun orderPaneljiHun = new OrderPaneljiHun();
+		orderPaneljiHun.setBounds(5, 5, 381, 548);
+		contentPane.add(orderPaneljiHun);
+	}
+
+}
